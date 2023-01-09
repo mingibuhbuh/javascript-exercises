f1e91d5f54f1dd423f3c516a8e124f94dc2b5bf5
mingibuhbuh <121769187+mingibuhbuh@users.noreply.github.com>
2023-01-09 13:33:10 -0500

    got it to pass the first check with nested for loops

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let i=0; i < array.length; i++){
+        for (let j=0; j<args.length;j++) {
+            if(args[j] === array[i]){
+                arrray = array.splice(i,1);
+        }
+    }
+    }
+    return array;
 
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+// npm test removeFromArray.spec.js
