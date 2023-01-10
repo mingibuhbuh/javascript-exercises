64b84befbbd3967bfcd68610a1e4f442bd200d13
mingibuhbuh <121769187+mingibuhbuh@users.noreply.github.com>
2023-01-10 13:42:33 -0500

    exercise 7 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = ((temp-32) * 5/9);
+  result = Math.round(temp * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = (temp * 9/5) + 32;
+  result = Math.round(temp * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
@@ -9,3 +15,5 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+// npm test tempConversion.spec.js
