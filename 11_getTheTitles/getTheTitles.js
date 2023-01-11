3dd32d794a66b9a676a7d2ddc6cce5401a222478
mingibuhbuh <121769187+mingibuhbuh@users.noreply.github.com>
2023-01-11 11:58:31 -0500

    11 done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let a = [];
+    arr.forEach(obj => {
+       a.push(obj.title); 
+    });
+    return a;
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+// npm test getTheTitles.spec.js
